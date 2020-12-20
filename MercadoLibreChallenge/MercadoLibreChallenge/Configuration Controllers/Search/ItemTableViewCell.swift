594637b6e51fafeb61390a7089acbda23a526afe
Salvador <salvattore315@gmail.com>
2020-12-20 12:25:24 -0500

    adding house screen with segue

---
+++ MercadoLibreChallenge/MercadoLibreChallenge/Configuration Controllers/Search/ItemTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ItemTableViewCell.swift
+//  MercadoLibreChallenge
+//
+//  Created by Salvador Martinez on 20/12/20.
+//
+
+import UIKit
+
+class ItemTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
